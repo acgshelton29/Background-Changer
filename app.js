6cdbd9e0d92a11d416a5ad2907951dcd4aab7552
Alec Shelton <ashelton2996@gmail.com>
2020-02-28 02:05:10 -0700

    The button now toggles the background color between lightblue and lightpink

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function colorChanger() {
-  document.body.style.background = 'lightpink';
+  //document.body.style.background = 'lightpink';
+  /*var color = document.body.style.background;
+  if(color.style.background == 'lightblue'){
+    color.style.background = 'lightpink';
+  } else {
+    color.style.background = 'lightblue';
+  }*/
+
+  var element = document.body;
+  console.log(element);
+
+  element.classList.toggle("lp-background");
 }
